c6255c2b2d89d8ab9ac62d69024e408cc6c4c1b3
Milosz Kaletka <milosz@example.com>
2025-11-22 01:09:24 +0100

    adding superuser

--- build.sh
+++ build.sh
@@ -7,12 +7,37 @@ set -o errexit
 # Zapewnia, że wszystkie pakiety są gotowe
 pip install -r requirements.txt
  
-# 2. Wykonanie Migracji
-# TO JEST KLUCZOWY KROK NAPRAWIAJĄCY BŁĄD 'no such table'
-python manage.py migrate
- 
 # 3. Opcjonalnie: Zbieranie plików statycznych (dla produkcji)
 # Jeśli używasz plików statycznych, Render tego potrzebuje
 python manage.py collectstatic --no-input
  
 # Koniec skryptu budowania
+
+# 2. Wykonanie Migracji
+# TO JEST KLUCZOWY KROK NAPRAWIAJĄCY BŁĄD 'no such table'
+python manage.py migrate
+ 
+# 1. Zdefiniuj zmienne środowiskowe
+export DJANGO_SUPERUSER_USERNAME="admin"
+export DJANGO_SUPERUSER_EMAIL="admin@example.com"
+export DJANGO_SUPERUSER_PASSWORD="admin1"
+ 
+# 2. Uruchom polecenie Basha
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+ 
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+ 
+if not User.objects.filter(username=username).exists():
+    user = User.objects.create_superuser(username=username, email=email)
+    user.set_password(password)
+    user.save()
+    print('Superużytkownik utworzony pomyślnie.')
+else:
+    print(f'Superużytkownik {username} już istnieje. Pomijanie.')
+"
+
